9a8b7cb6c04e5fdc4df3c8c20c7532bf0b2c4e8e
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2024-02-01 20:20:31 +0530

    footer header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Navbar , Button } from 'flowbite-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <Navbar className='border-b-2'>
+    <div className='flex items-center justify-around w-full'>
+    <div className=''>
+        <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="text-blue-500">Company</span>
+        <span className="text-gray-900">Logo</span>
+      </Link>
+      </div>
+        <div className="" >
+      <Link to={"/"}>
+          <Button gradientDuoTone={"purpleToBlue"} pill outline>
+            Start Selling
+          </Button>
+        </Link>
+        
+        </div>
+    </div>
+        
+      </Navbar>
+    
+  )
+}
+
+export default Header
