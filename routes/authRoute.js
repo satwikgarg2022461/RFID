b73b8941232b55f1ff1a03b87e3e0a71a96a7927
Saurav <saurav22464@iiitd.ac.in>
2024-03-13 20:08:56 +0530

    added login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,13 @@
 import express from "express";
-import {registerController} from '../controllers/authController.js'
+import {registerController,loginController,testController} from '../controllers/authController.js'
+import { isAdmin, requieSignIn } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/register',registerController);
+
+router.post('/login',loginController);
+
+router.get('/test',requieSignIn,isAdmin,testController);
 
 export default router;
